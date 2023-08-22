b17f8db33a5a9ec10321ca91748ab6f0da708638
potarohblog <potarohblog@gmail.com>
2023-08-22 16:46:12 +0900

    userdata4

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :items
-  has_many :orders
+        #  出品機能
+  # has_many :items
+  # 購入機能
+  # has_many :orders
 
   with_options presence: true do
     validates :nickname
