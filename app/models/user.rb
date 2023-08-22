d523afaae79b517aa28e6de2f682b823b4d29170
potarohblog <potarohblog@gmail.com>
2023-08-22 20:31:42 +0900

    userdata5

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,25 +3,40 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-        #  出品機能
-  # has_many :items
-  # 購入機能
-  # has_many :orders
 
-  with_options presence: true do
-    validates :nickname
+        #  validate do
+        #   password_complexity
+        #  end
 
-    with_options format: {with: /\A[ぁ-んァ-ン一-龥]/} do
-      validates :last_name
-      validates :first_name
-    end
+        # 出品機能
+        # has_many :items
 
-    with_options format: {with: /\A[ァ-ヶー－]+\z/} do
-      validates :last_name_kana
-      validates :first_name_kana
-    end
-
-    validates :birthday
-  end
-
+        # 購入機能
+        # has_many :orders
+        
+        # ニックネームが空欄だと入力できないようにする
+        with_options presence: true do
+          validates :nickname
+          validates :birthday
+          
+          with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+            validates :last_name
+            validates :first_name
+          end
+          
+          with_options format: {with: /\A[ァ-ヶー－]+\z/} do
+            validates :last_name_kana
+            validates :first_name_kana
+          end
+      
+          
+        end
+      
+        private
+      
+        def password_complexity
+          if password.present? && !password.match?(/\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/)
+            errors.add(:password, "は英字と数字の両方を含めて設定してください")
+          end
+        end
 end
--- spec/factories/payments.rb
+++ spec/factories/payments.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :payment do
+  # factory :payment do
     
-  end
+  # end
 end
