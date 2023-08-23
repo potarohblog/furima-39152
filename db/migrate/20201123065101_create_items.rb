8188dad76d42215d9f9925e19d60f2c39fbadd07
potarohblog <potarohblog@gmail.com>
2023-08-23 23:32:33 +0900

    display1

--- db/migrate/20201123065101_create_items.rb
+++ db/migrate/20201123065101_create_items.rb
@@ -1,16 +1,17 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      # t.references :user,              null: false, foreign_key: true
-      # t.string     :name,              null: false
-      # t.text       :description,       null: false
-      # t.integer    :category_id,       null: false
-      # t.integer    :item_status_id,    null: false
-      # t.integer    :shipping_cost_id,  null: false
-      # t.integer    :prefecture_id,     null: false
-      # t.integer    :shipping_date_id,  null: false
-      # t.integer    :price,             null: false
-      # t.timestamps
+      t.references :user,                   null: false, foreign_key: true
+      t.string     :title,                  null: false
+      t.text       :text,                   null: false
+      t.integer    :category_id,            null: false
+      t.integer    :sales_status_id,        null: false
+      t.integer    :shipping_cost_id,       null: false
+      t.integer    :prefecture_id,          null: false
+      t.integer    :scheduled_delivery_id,  null: false
+      t.integer    :shipping_date_id,       null: false, default: 1
+      t.integer    :price,                  null: false
+      t.timestamps
     end
   end
 end
