7049d9d5de4c62c37242abc4d46a8cfd3c60c061
potarohblog <potarohblog@gmail.com>
2023-08-24 19:27:11 +0900

    display4

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,23 +1,21 @@
 class Item < ApplicationRecord
-    extend ActiveHash::Associations::ActiveRecordExtensions
-    belongs_to :user
-    has_one :order
-    has_one_attached :image
-    belongs_to_active_hash :category
-    belongs_to_active_hash :sales_status
-    belongs_to_active_hash :shipping_cost_status
-    belongs_to_active_hash :prefecture
-    belongs_to_active_hash :scheduled_delivery
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
+  # has_one :order
+  has_one_attached :image
+  belongs_to_active_hash :category
+  belongs_to_active_hash :sales_status
+  belongs_to_active_hash :shipping_cost_status
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :scheduled_delivery
   
-    with_options presence: true do
-      validates :title, :text, :image
-      
-      with_options numericality: { other_than: 0 } do
-        validates :category_id, :sales_status_id, :shipping_cost_id, :prefecture_id, :scheduled_delivery_id
-      end
-  
-      with_options format: { with: /\A[0-9]+\z/ } do
-        validates :price, numericality: { greater_than: 300, less_than: 9999999 } 
-      end
+  with_options presence: true do
+    validates :title, :text, :image
+    
+    with_options numericality: { other_than: 0 } do
+      validates :category_id, :sales_status_id, :shipping_cost_id, :prefecture_id, :scheduled_delivery_id
     end
+    
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, only_integer: true }
   end
+end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -132,16 +132,28 @@ describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       
+      it '価格に半角数字以外が含まれている場合は出品できない' do
+        @item.price = "abc"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
+      it '価格に全角数字が含まれている場合は出品できない' do
+        @item.price = "１２３４５６"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
       it '価格の範囲が、300円未満だと出品できない' do
         @item.price = 100
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be greater than 300")
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
       
       it '価格の範囲が、9,999,999円を超えると出品できない' do
         @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be less than 9999999")
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
     end
   end
