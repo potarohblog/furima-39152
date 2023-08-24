f973ad0f5b1541c06ba4830de422fbd18c9b1282
potarohblog <potarohblog@gmail.com>
2023-08-24 21:08:29 +0900

    list2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -41,8 +41,9 @@ class ItemsController < ApplicationController
   #   end
   # end
 
-  # def show
-  # end
+  def show
+    @item = Item.find(params[:id])
+  end
 
   # def destroy
   #   # ログインしているユーザーと同一であればデータを削除する
