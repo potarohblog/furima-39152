c7851a4bed0d69e1bc3bcb8e1926afc848ef484e
potarohblog <potarohblog@gmail.com>
2023-08-29 14:09:25 +0900

    deploy5

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -29,6 +29,7 @@ class OrdersController < ApplicationController
     elsif @item.user == current_user
         redirect_to root_path
     end
+  end
     
   def order_params
     params.require(:order_form).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
